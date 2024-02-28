43d38c810d042f1492f638e211a0a21c379dad71
AllenC <allenstivenc@gmail.com>
2024-02-28 17:24:13 -0500

    Primer avance

---
+++ src/Operaciones/Combinatoria.java
@@ -0,0 +1,11 @@
+package Operaciones;
+
+public class Combinatoria {
+	public double calcular (double num1, double num2) throws Exception{
+		if(num2<=num1) {
+			
+		}else {
+			throw new Exception("El numero que representa r! no puede ser mayor que n!");
+		}
+	}
+}
